df3cad016080545a98ea24305407bda4ccb017b9
Julien Thierry <julienthierrypro@gmail.com>
2022-10-15 13:35:31 +0200

    Login

--- react-firebase/src/components/Login.js
+++ react-firebase/src/components/Login.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useRef } from 'react';
+import { auth } from '../utils/firebase.config';
 
 const Login = () => {
+    const loginEmail = useRef();
+    const loginPassword = useRef();
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            const user = await signInWithEmailAndPassword(
+                auth,
+                loginEmail.current.value,
+                loginPassword.current.value,
+            );
+        } catch (error) {
+            console.log(error.message);
+        }
+
+    }
+
     return (
-        <div>
-            <h1>LOGIN</h1>
+        <div className="login-container">
+            <div className="login">
+                <h3>Se connecter</h3>
+                <form className="form-login" onSubmit={(e) => handleLogin(e)}>
+                    <input type="email" placeholder='Email' required ref={loginEmail} />
+                    <input type="password" placeholder='Mot de passe' required ref={loginPassword} />
+                    <input type="submit" value="Se connecter" />
+                </form>
+            </div>
         </div>
     );
 };
